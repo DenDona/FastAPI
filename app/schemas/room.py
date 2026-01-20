6b274998812a0cc68154aab5913e05bff22985a2
DenDona <danilasamp2@mail.ru>
2026-01-20 22:43:39 +0300

    added schemas rooms

---
+++ app/schemas/room.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+class RoomCreate(BaseModel):
+    name: str = Field(..., min_length=1, max_length=100)
+    capacity: Annotated[int, Field(gt=0, description="Вместимость должна быть больше 0")]
+    has_projector: bool
+
+class RoomResponse(RoomCreate):
+    id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
