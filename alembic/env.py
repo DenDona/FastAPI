0a8238c476a0c1f1988354e3da07f9b2d52b3a45
DenDona <danilasamp2@mail.ru>
2026-01-20 22:48:11 +0300

    added alembic and migrate rooms

---
+++ alembic/env.py
@@ -0,0 +1,57 @@
+import asyncio
+import os
+from logging.config import fileConfig
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import create_async_engine
+from alembic import context
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+from app.models.base import Base
+from app.models.room import Room
+# from app.models.booking import Booking
+
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set in environment")
+
+target_metadata = Base.metadata
+
+def run_migrations_offline():
+    context.configure(
+        url=DATABASE_URL,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+def do_run_migrations(connection: Connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_async_migrations():
+    connectable = create_async_engine(
+        DATABASE_URL,
+        poolclass=pool.NullPool,
+    )
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+    await connectable.dispose()
+
+def run_migrations_online():
+    asyncio.run(run_async_migrations())
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
