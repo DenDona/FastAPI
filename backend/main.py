952ec2ba5ae94cfc01f1b63d0f65abee8d9ee99d
DenDona <142161687+DenDona@users.noreply.github.com>
2026-03-19 23:23:56 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 import datetime
-
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from fastapi import FastAPI
 from app.api.v1.room import router as rooms_router
 from app.api.v1.booking import router as booking_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://my-app-frontend.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(rooms_router)
 app.include_router(booking_router)
 
